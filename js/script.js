9c8e4be751707320fe0bf9daead776e84cf83cf3
Vlada Gorpinenko <vlada.san.05@yandex.ru>
2021-08-26 09:57:59 +0300

    Присвоила значения

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 'use script';
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+console.log('Доход за месяц:', money);
 
 
-alert('Hello my friend');
-console.log('Bye my friend');
+let income = 'Дополнительный доход:';
+console.log(income + ' 10000');
+
+
+let addExpenses = 'Интернет, такси, коммуналка, продукты';
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(addExpenses.length);
+
+
+let deposit = true;
+console.log(deposit);
+
+
+let mission = 100000;
+let period = 3;
+console.log('Период равен', period, 'месяцев.', 'Цель заработать', mission, 'рублей.');
+
+
+let budgetDay = 'Дневной бюджет:';
+console.log(budgetDay, money/=30);
